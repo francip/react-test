f7ddc48f2b5eab215d78819c718542e0c26f466c
Franci Penov <francip@gmail.com>
2024-02-26 19:49:06 -0800

    type=module + esnext

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,42 @@
-const path = require("path");
+import path from "path";
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
-    entry: "./src/index.tsx",
-    resolve:
-    {
-       extensions: [ '.tsx', '.ts', '.jsx', '.js' ],
-    },
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "bundle.js",
-    },
-    devServer: {
-        port: 8080,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader',
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
+export default function config(env, argv) {
+    return {
+        entry: "./src/index.tsx",
+        resolve: {
+            extensions: [".tsx", ".ts", ".jsx", ".js"],
+        },
+        output: {
+            path: path.join(process.cwd(), "/dist"),
+            filename: "bundle.js",
+        },
+        devServer: {
+            port: 8080,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                },
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    loader: "ts-loader",
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(process.cwd(), "/public/index.html"),
+            }),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "/public/index.html"),
-        }),
-    ],
-};
+    };
+}
